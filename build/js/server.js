0e2a87e3ff05fc84b0c5963d63fe43ee695db8dc
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2014-10-21 18:00:56 +0900

    git ignore edited

---
+++ build/js/server.js
@@ -0,0 +1,32 @@
+(function() {
+  var http, io, server, socketIO;
+
+  http = require("http");
+
+  socketIO = require("socket.io");
+
+  server = http.createServer(function(req, res) {}).listen(3333);
+
+  io = socketIO.listen(server);
+
+  console.log("start node");
+
+  console.log(io);
+
+  io.sockets.on("connection", function(socket) {
+    console.log("connect!");
+    socket.on("toAll", function(data) {
+      io.sockets.emit("toClient", {
+        value: data.value
+      });
+    });
+    socket.on("toServerBroad", function(data) {
+      console.log(data);
+      socket.broadcast.emit("toClient", {
+        value: data.value
+      });
+    });
+    socket.on("disconnect", function() {});
+  });
+
+}).call(this);
